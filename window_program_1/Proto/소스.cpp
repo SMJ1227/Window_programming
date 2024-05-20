df6485d308a75a35910b8793c7bb26370b9a346c
SMJ1227 <112992077+SMJ1227@users.noreply.github.com>
2024-05-20 18:19:50 +0900

    proto

---
+++ window_program_1/Proto/소스.cpp
@@ -0,0 +1,115 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <windows.h>
+#include <tchar.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+//#include "resource.h"
+
+#define WINDOW_X 300
+#define WINDOW_Y 600
+#define GRID 50
+#define GRID_X 6
+#define GRID_Y 12
+#define M_PI 3.14
+#define TIMER_ID 1
+
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = L"Window Class Name";
+LPCTSTR lpszWindowName = L"Window Programming";
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
+	HWND hWnd;
+	MSG Message;
+	WNDCLASSEX WndClass;
+	g_hInst = hInstance;
+	srand((unsigned int)time(NULL));
+	WndClass.cbSize = sizeof(WndClass);
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = (WNDPROC)WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hCursor = LoadCursor(NULL, IDC_HAND);
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.lpszMenuName = MAKEINTRESOURCE(NULL);
+	WndClass.lpszClassName = lpszClass;
+	WndClass.hIconSm = LoadIcon(NULL, IDI_QUESTION);
+	RegisterClassEx(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszWindowName, WS_OVERLAPPEDWINDOW, 0, 0, 315, 658, NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	while (GetMessage(&Message, 0, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return Message.wParam;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM IParam) {
+	HDC hDC, mDC;
+	PAINTSTRUCT ps;
+	HBITMAP hBitmap;
+	RECT rt;
+
+	//--- 메세지 처리하기
+	switch (uMsg) {
+	case WM_CREATE:
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam)) 
+		{
+		}
+		InvalidateRect(hWnd, NULL, FALSE);
+		break;
+
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_RETURN:
+			break;
+		case VK_RIGHT:
+			break;
+		case VK_LEFT:
+			break;
+		case VK_DOWN:
+			break;
+		}
+	case WM_TIMER:
+		switch (wParam)
+		{
+		case TIMER_ID:
+			break;
+		}
+		InvalidateRect(hWnd, NULL, FALSE);
+		break;
+	case WM_PAINT:
+	{
+		GetClientRect(hWnd, &rt);
+		hDC = BeginPaint(hWnd, &ps);
+		mDC = CreateCompatibleDC(hDC);
+		hBitmap = CreateCompatibleBitmap(hDC, rt.right, rt.bottom);
+		SelectObject(mDC, (HBITMAP)hBitmap);
+		//--- 모든 그리기를 메모리 DC에한다.
+		
+		//--- 마지막에 메모리 DC의 내용을화면DC로복사한다.
+		BitBlt(hDC, 0, 0, rt.right, rt.bottom, mDC, 0, 0, SRCCOPY);
+		DeleteDC(mDC);
+		DeleteObject(hBitmap);
+		EndPaint(hWnd, &ps);
+		break;
+	}
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return DefWindowProc(hWnd, uMsg, wParam, IParam);		//---위의 세 메시지 외의 나머지 메세지는 OS로
+}
